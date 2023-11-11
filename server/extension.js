2eaa983eb9e9a19090d48b66952cc771811562b0
Manu <manu@mercenaries.ai>
2023-11-11 15:07:21 +0800

    switching to regular build (without SHIM)

--- server/extension.js
+++ server/extension.js
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2023 MERCENARIES.AI PTE. LTD.
- * All rights reserved.
- */
-
-
-await(async()=>{let{dirname:e}=await import("path"),{fileURLToPath:i}=await import("url");if(typeof globalThis.__filename>"u"&&(globalThis.__filename=i(import.meta.url)),typeof globalThis.__dirname>"u"&&(globalThis.__dirname=e(globalThis.__filename)),typeof globalThis.require>"u"){let{default:a}=await import("module");globalThis.require=a.createRequire(import.meta.url)}})();
-
-
 // component_LlmQuery.js
 import { createComponent, queryLlmByModelId, DEFAULT_LLM_MODEL_ID, getLlmQueryInputs, LLM_QUERY_OUTPUT, LLM_QUERY_CONTROL } from "../../../src/utils/omni-utils.js";
 var GROUP_ID = "text_generation";
